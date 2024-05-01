0d1a773f181802691cb6594ec61f5ddd73976385
iahsanGill <mgill.bese22seecs@seecs.edu.pk>
2024-05-02 00:24:42 +0500

    Add the github-actions workflow

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateProductDto, ReviewDto } from './dto/create-product.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Product, Review } from './model/products.model';
@@ -10,54 +10,90 @@ export class ProductsService {
   ) {}
 
   async getOne(id: string) {
-    return await this.productModel.findByPk(id, { include: [Review] });
+    try {
+      return await this.productModel.findByPk(id, { include: [Review] });
+    } catch (error) {
+      throw new BadRequestException('Failed to get the product', {
+        cause: error,
+      });
+    }
   }
 
   async getAll() {
-    return await this.productModel.findAll();
+    try {
+      return await this.productModel.findAll();
+    } catch (error) {
+      throw new BadRequestException('Failed to get the products', {
+        cause: error,
+      });
+    }
   }
 
   async create(createProductDto: CreateProductDto) {
-    return await this.productModel.create(createProductDto);
+    try {
+      return await this.productModel.create(createProductDto);
+    } catch (error) {
+      throw new BadRequestException('Failed to create the product', {
+        cause: error,
+      });
+    }
   }
 
   async addReview(reviewDto: ReviewDto, id: string) {
-    // Create a new review and associate it with the product
-    await Review.create({ ...reviewDto, productId: id });
+    try {
+      // Create a new review and associate it with the product
+      await Review.create({ ...reviewDto, productId: id });
 
-    // Find the product by its ID and include its associated reviews
-    const product = await this.productModel.findByPk(id, {
-      include: [Review],
-    });
+      // Find the product by its ID and include its associated reviews
+      const product = await this.productModel.findByPk(id, {
+        include: [Review],
+      });
 
-    // Calculate the updated number of reviews and average rating
-    const numReviews = product.reviews.length;
-    const totalRating = product.reviews.reduce(
-      (sum, review) => sum + review.rating,
-      0,
-    );
-    const averageRating = numReviews > 0 ? totalRating / numReviews : 0;
+      // Calculate the updated number of reviews and average rating
+      const numReviews = product.reviews.length;
+      const totalRating = product.reviews.reduce(
+        (sum, review) => sum + review.rating,
+        0,
+      );
+      const averageRating = numReviews > 0 ? totalRating / numReviews : 0;
 
-    // Update the product's numReviews and rating fields
-    product.numReviews = numReviews;
-    product.rating = averageRating;
+      // Update the product's numReviews and rating fields
+      product.numReviews = numReviews;
+      product.rating = averageRating;
 
-    // Save the updated product
-    await product.save();
+      // Save the updated product
+      await product.save();
 
-    // Return the updated product
-    return product;
+      // Return the updated product
+      return product;
+    } catch (error) {
+      throw new BadRequestException('Failed to create the review', {
+        cause: error,
+      });
+    }
   }
 
   async update(updateProductDto: CreateProductDto, id: string) {
-    return await this.productModel.update(updateProductDto, {
-      where: { id },
-    });
+    try {
+      return await this.productModel.update(updateProductDto, {
+        where: { id },
+      });
+    } catch (error) {
+      throw new BadRequestException('Failed to update the product', {
+        cause: error,
+      });
+    }
   }
 
   async delete(id: string) {
-    return await this.productModel.destroy({
-      where: { id },
-    });
+    try {
+      return await this.productModel.destroy({
+        where: { id },
+      });
+    } catch (error) {
+      throw new BadRequestException('Failed to delete the product', {
+        cause: error,
+      });
+    }
   }
 }
